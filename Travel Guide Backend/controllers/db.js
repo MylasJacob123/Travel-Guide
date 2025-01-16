32637ac775c9c84e8ca6baa1cbc1bce1f2429a51
peacendebele <peacendebele073@gmail.com>
2025-01-16 12:05:02 +0200

    New changes to pull

--- Travel Guide Backend/controllers/db.js
+++ Travel Guide Backend/controllers/db.js
@@ -2,95 +2,9 @@ const {
   collection,
   addDoc,
   getDocs,
-  deleteDoc,
-  doc,
-  setDoc,
 } = require("firebase/firestore");
 const { db } = require("../config/firebase");
 
-const addUser = async (req, res) => {
-  const { firstname, lastname, email, password } = req.body;
-
-  try {
-    const docRef = await addDoc(collection(db, "users"), {
-      firstname,
-      lastname,
-      email,
-      password,
-    });
-    res.json({
-      message: "User added successfully",
-    });
-  } catch (error) {
-    console.log("Adding user error", error);
-    res.status(500).json({ error: "Failed to add user" });
-  }
-};
-
-const getUsers = async (req, res) => {
-  try {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    const data = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    res.json({
-      data,
-    });
-  } catch (error) {
-    console.log("Error fetching users", error);
-    res.status(500).json({ error: "Error fetching users" });
-  }
-};
-
-const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { firstname, lastname, email, password } = req.body;
-
-    console.log("Updating user ID:", id);
-
-    if (!firstname || !lastname || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
-    const userDocRef = doc(db, "users", id);
-
-    await setDoc(
-      userDocRef,
-      {
-        firstname,
-        lastname,
-        email,
-        password,
-      },
-      { merge: true }
-    );
-
-    res.json({
-      message: "User updated successfully",
-    });
-  } catch (error) {
-    console.log("Error updating user", error);
-    res.status(500).json({ error: "Failed to update user" });
-  }
-};
-
-const deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    console.log("Deleting user with ID:", id);
-    const userDocRef = doc(db, "users", id);
-    await deleteDoc(userDocRef);
-
-    res.json({
-      message: "User successfully deleted",
-    });
-  } catch (error) {
-    console.log("Error deleting user", error);
-    res.status(500).json({ error: "Failed to delete user" });
-  }
-};
 
 const getUserFavorite = async (req, res) => {
   const { userId } = req.params;
@@ -129,10 +43,6 @@ const addUserFavorite = async (req, res) => {
 
 
 module.exports = {
-  addUser,
-  getUsers,
-  deleteUser,
-  updateUser,
   getUserFavorite,
   addUserFavorite
 };
